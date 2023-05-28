b4309a33769e3d45800bfe00c5fb4284c6d1ad08
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-05-28 20:09:45 -0300

    Aprdendo Angular; estruturas do Angular; Criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirtsComponentComponent } from './components/firts-component/firts-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirtsComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/firts-component/firts-component.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirtsComponentComponent } from './firts-component.component';
+
+describe('FirtsComponentComponent', () => {
+  let component: FirtsComponentComponent;
+  let fixture: ComponentFixture<FirtsComponentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FirtsComponentComponent]
+    });
+    fixture = TestBed.createComponent(FirtsComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
