fef4ab374b575686678790002c691527fa18dc61
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-06-03 11:16:10 -0300

    adidionado input em componente filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Maria Arlyanne';
+
+  dadosUsuario = {
+    email: 'mariaarlyanne@teste.com.br',
+    role: 'Estudante/Programadora',
+    instituicao: 'Unifametro',
+    idade: '28',
+    altura: '1.59',
+    cor: 'Parda,',
+    estadoCivil: 'Namorando',
+    corPreferida: 'Vermelha',
+    time: 'Flamengo',
+    hobbies: 'Estudar, Jogar bola, ler livros'
+
+
+
+
+
+  }  
+  
   title = 'curso-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirtsComponentComponent } from './components/firts-component/firts-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirtsComponentComponent
+    FirtsComponentComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule
